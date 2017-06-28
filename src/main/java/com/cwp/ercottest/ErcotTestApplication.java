33f02a3ca3862666cf4652348e400543c6f792fe
pierreolivierroy <roy.pierre.o@gmail.com>
2017-06-27 20:26:57 -0400

    Initial commit: with hibernate configuration

---
+++ src/main/java/com/cwp/ercottest/ErcotTestApplication.java
@@ -0,0 +1,12 @@
+package com.cwp.ercottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErcotTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErcotTestApplication.class, args);
+	}
+}
